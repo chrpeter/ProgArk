275a2fdacb6b368265119d9a59394bdab11d0de6
Christian Peter <christian150790@gmail.com>
2014-01-13 14:35:52 +0100

    initial commit

---
+++ Exercise1/Exercise1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Exercise1
+//
+//  Created by Christian Peter on 13.01.14.
+//  Copyright (c) 2014 Christian&Simen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Exercise1/Exercise1/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Exercise1
+//
+//  Created by Christian Peter on 13.01.14.
+//  Copyright (c) 2014 Christian&Simen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
