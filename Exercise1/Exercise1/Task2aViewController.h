1490bc2eccb221484e52f4775437c96c02d35ec0
simen skoglund <simen.skoglund@gmail.com>
2014-01-13 18:08:34 +0100

    starta på opg2

---
+++ Exercise1/Exercise1/Task2aViewController.h
@@ -0,0 +1,13 @@
+//
+//  Task2aViewController.h
+//  Exercise1
+//
+//  Created by Simen Skoglund on 13.01.14.
+//  Copyright (c) 2014 Christian&Simen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Task2aViewController : UIViewController
+
+@end
---
+++ Exercise1/Exercise1/Task2aViewController.m
@@ -0,0 +1,38 @@
+//
+//  Task2aViewController.m
+//  Exercise1
+//
+//  Created by Simen Skoglund on 13.01.14.
+//  Copyright (c) 2014 Christian&Simen. All rights reserved.
+//
+
+#import "Task2aViewController.h"
+
+@interface Task2aViewController ()
+
+@end
+
+@implementation Task2aViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
