898620a58b13301c2f924b9232ca7c3820a9e686
simen skoglund <simen.skoglund@gmail.com>
2014-01-15 02:37:31 +0100

    Nesten ferdig med task 2, rotate ikke optimal

--- Exercise1/Exercise1/Task2aViewController.m
+++ Exercise1/Exercise1/Task2aViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "Task2aViewController.h"
+#include <math.h>
 
 @interface Task2aViewController ()
 
@@ -23,10 +24,6 @@
     return self;
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-}
 
 - (void)didReceiveMemoryWarning
 {
@@ -39,32 +36,36 @@
     UITouch *touch = [[event allTouches] anyObject];
     CGPoint location = [touch locationInView:touch.view];
     Helicopter.center = location;
+
+
 }
 
 -(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
 {
+    //Change the constants to make the randomization higher.
+    int newWidth = arc4random() % 70;
+    int newHeight = arc4random() % 30;
+    newHeight += 50; //to make sure the helicopter isnt getting to small
+    newWidth += 50;
+    
+    NSString *xCoord = [[NSNumber numberWithFloat:Helicopter.frame.origin.x] stringValue];
+    NSString *yCoord = [[NSNumber numberWithFloat:Helicopter.frame.origin.y] stringValue];
+    
+    NSString *d = [NSString stringWithFormat:@"x:%@, y:%@", xCoord, yCoord];
+    //Setter textlabel til riktige coordinater.
+    labelResult.text = d;
+    //Random size
+    Helicopter.frame = CGRectMake(0, 0, newHeight, newWidth);
+
     [self touchesBegan:touches withEvent:event];
     
-    
 }
 
--(void)HelicopterMovement{
-    
-    Helicopter.center = CGPointMake(Helicopter.center.x + X, Helicopter.center.y + Y);
-    
-    if(Helicopter.center.x < 60){
-        X = 0 - X;
-    }
-    if(Helicopter.center.x > 260){
-        X = 0 - X;
-    }
-    if(Helicopter.center.y < 50){
-        Y = 0 - Y;
-    }
-    if(Helicopter.center.y > 550){
-        Y = 0 - Y;
-        
-    }
-}
+//Roterer 90 grader per trykk
+//Har ikke klart å lage hvordan man roterer om sitt eget imageview, kun startpointet.
+-(IBAction)rotateButton:(id)sender{
+    transform += M_PI/2;
+    Helicopter.transform = CGAffineTransformMakeRotation(transform);
 
+}
 @end
